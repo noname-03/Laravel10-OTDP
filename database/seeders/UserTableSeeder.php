0109d00129e12ef4bf8b1c39af3ddc36baa32c9a
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-07-13 16:09:40 +0700

    add laravel spatie permission

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
+            'remember_token' => Str::random(10),
+        ]);
+        Role::create(['name' => 'admin']);
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@mail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
+            'remember_token' => Str::random(10),
+        ]);
+        Role::create(['name' => 'user']);
+        $user->assignRole('user');
+    }
+}
